33203ff3a1d5f5a4576d5d5dae31e8b49e528e86
Oleksandr Cherevko <oc.oleksandr.cherevko@outlook.com>
2023-01-16 17:49:07 +0200

    task resolved

--- Homework_15/Program.cs
+++ Homework_15/Program.cs
@@ -1,54 +1,24 @@
 ﻿
-
-/*
-Создать класс со следующими методами:
-Add - принимает значение любого типа (который реализует IComparable<T>) 
-    и сохраняет его в коллекцию (List)
-IsGreaterOrEqual - принимает значение любого типа 
-    (который реализует IComparable<T>) и возвращает true если оно больше 
-        или равно чем каждый элемент коллекции и false если нет
-
-Использовать обобщения
-Организовать сравнение с помощью IComparable<T>. 
-Его метод CompareTo возвращает число меньше 0 если класс меньше,
-    0 - равен и больше 0 если он больше чем другой.
-*/
-
-
-
-
-class MyClass<T> : IComparable<T>
+class MyClass<T> where T : IComparable<T>
 {
-    List<T> list = new List<T>();
 
+    private List<T> list = new List<T>();
 
     public void Add(T t)
     {
         list.Add(t);
     }
 
-    public int CompareTo(T? other)
+    public bool isGreaterOrEqual(T t)
     {
-        if (/*code here*/false)
+        foreach (var item in list)
         {
-            return 0;
+            if (t.CompareTo(item) < 0)
+            {
+                return false;
+            }
         }
-        else if (/*code here*/true)
-        {
-            return -1;
-        }
-        else
-        {
-            return 1;
-        }
-    }
-
-
-
-    public bool isGreaterOrEqual(MyClass<T> t)
-    {
-        //code here
-        return false;
+        return true;
     }
 }
 
@@ -60,7 +30,12 @@ class Program
 
         MyClass<int> myClass = new MyClass<int>();
 
+        myClass.Add(1);
+        myClass.Add(2);
 
+        Console.Write("Enter value: ");
+        int value = int.Parse(Console.ReadLine());
 
+        Console.WriteLine(myClass.isGreaterOrEqual(value));
     }
 }
